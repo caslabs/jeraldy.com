3a5218d31cc5d935f70f807f119ff34eb59f5502
jerald <jeraldy@hawaii.edu>
2023-11-01 09:42:12 -1000

    blogs redirection patch

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,9 +20,9 @@ export default async function ProjectsPage() {
 		return acc;
 	}, {} as Record<string, number>);
 
-	const featured = allProjects.find((project) => project.slug === "unkey")!;
-	const top2 = allProjects.find((project) => project.slug === "planetfall")!;
-	const top3 = allProjects.find((project) => project.slug === "highstorm")!;
+	const featured = allProjects.find((project) => project.slug === "caslabs.ai")!;
+	const top2 = allProjects.find((project) => project.slug === "jobs")!;
+	const top3 = allProjects.find((project) => project.slug === "simulacrae.com")!;
 	const sorted = allProjects
 		.filter((p) => p.published)
 		.filter(
