b4e262f3fe0df84db6f50d28f6c2d11c50a3db08
jerald <jeraldy@hawaii.edu>
2023-11-01 09:21:44 -1000

    removed animations

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,12 @@ import { Footer } from "./components/footer";
 
 const navigation = [
 	{ name: "Projects", href: "/projects" },
-	{ name: "LinkedIn", href: "/contact" },
 ];
 
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-			<nav className="my-16 animate-fade-in">
+			<nav className="my-16">
 				<ul className="flex flex-wrap items-center justify-center gap-4">
 					{navigation.map((item) => (
 						<Link
@@ -22,6 +21,14 @@ export default function Home() {
 							{item.name}
 						</Link>
 					))}
+						<a
+                        href="https://www.linkedin.com/in/jeraldy-cascayan-695112190/" // Update the path to your resume
+                        className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    	>
+                        	LinkedIn
+                    	</a>
 
 						<a
                         href="resume.pdf" // Update the path to your resume
@@ -33,17 +40,15 @@ export default function Home() {
                     	</a>
 				</ul>
 			</nav>
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<Particles
-				className="absolute inset-0 -z-10 animate-fade-in"
+				className="absolute inset-0 -z-10"
 				quantity={100}
 			/>
-			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+			<h1 className="z-10 text-4xl text-transparent bg-white cursor-default text-edge-outline font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
 				jeraldy
 			</h1>
 
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-			<div className="my-16 text-center animate-fade-in">
+			<div className="my-16 text-center">
 				<h2 className="text-sm text-zinc-500 mx-6">
 					Hi, my name is jeraldy (he/him), I'm an aspiring software engineer and I like to write about AI research at
 					{" "}
